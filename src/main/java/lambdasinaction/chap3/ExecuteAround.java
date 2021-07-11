b79b538c15d651f00783c8314423a96741bfbc10
moqi <moqimoqidea@gmail.com>
2021-07-11 21:23:49 +0800

    Update data.txt path

--- src/main/java/lambdasinaction/chap3/ExecuteAround.java
+++ src/main/java/lambdasinaction/chap3/ExecuteAround.java
@@ -21,14 +21,14 @@ public class ExecuteAround {
 
     public static String processFileLimited() throws IOException {
         try (BufferedReader br =
-                     new BufferedReader(new FileReader("lambdasinaction/chap3/data.txt"))) {
+                     new BufferedReader(new FileReader("src/main/resources/lambdasinaction/chap3/data.txt"))) {
             return br.readLine();
         }
     }
 
 
 	public static String processFile(BufferedReaderProcessor p) throws IOException {
-		try(BufferedReader br = new BufferedReader(new FileReader("lambdasinaction/chap3/data.txt"))){
+		try(BufferedReader br = new BufferedReader(new FileReader("src/main/resources/lambdasinaction/chap3/data.txt"))){
 			return p.process(br);
 		}
 
