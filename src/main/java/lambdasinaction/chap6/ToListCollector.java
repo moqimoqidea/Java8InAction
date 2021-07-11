8fa80af00538d4f6e06136db5bd82dae223f98d2
moqi <moqimoqidea@gmail.com>
2021-07-11 22:36:39 +0800

    Update ToListCollector.java

--- src/main/java/lambdasinaction/chap6/ToListCollector.java
+++ src/main/java/lambdasinaction/chap6/ToListCollector.java
@@ -3,23 +3,24 @@ package lambdasinaction.chap6;
 import java.util.*;
 import java.util.function.*;
 import java.util.stream.Collector;
+
 import static java.util.stream.Collector.Characteristics.*;
 
 public class ToListCollector<T> implements Collector<T, List<T>, List<T>> {
 
     @Override
     public Supplier<List<T>> supplier() {
-        return () -> new ArrayList<T>();
+        return ArrayList::new;
     }
 
     @Override
     public BiConsumer<List<T>, T> accumulator() {
-        return (list, item) -> list.add(item);
+        return List::add;
     }
 
     @Override
     public Function<List<T>, List<T>> finisher() {
-        return i -> i;
+        return Function.identity();
     }
 
     @Override
